f610aff0646727be6753321d933029e4b1c5f05f
Carlos29SA <47302474+Carlos29SA@users.noreply.github.com>
2020-02-11 10:55:10 +0100

    Initial commit

---
+++ src/main/java/negocio/grupo/GrupoSA.java
@@ -0,0 +1,15 @@
+package negocio.grupo;
+
+import negocio.empleado.TTrabaja;
+
+import java.util.List;
+
+public interface GrupoSA {
+	void insertar(TGrupo grupo) throws Exception;
+	TGrupo mostrar(int id) throws Exception;
+	List<TGrupo> mostrarTodos() throws Exception;
+	void modificar(TGrupo grupo) throws Exception;
+	void eliminar(int id) throws Exception;
+	void insertarEmpleado(TTrabaja empleado) throws Exception;
+	void eliminarEmpleado(TTrabaja empleado) throws Exception;
+}
